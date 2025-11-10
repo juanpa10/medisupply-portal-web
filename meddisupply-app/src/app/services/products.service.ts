a45cd79614e41bdb64526ec1d849ce411906d9d7
Juan Pablo Camacho Caro <jp.camacho10@uniandes.edu.co>
2025-11-10 15:48:26 -0500

    productos en csv

--- meddisupply-app/src/app/services/products.service.ts
+++ meddisupply-app/src/app/services/products.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -15,5 +15,13 @@ export class ProductsService {
 
   getProducts(page: number = 1, perPage: number = 10): Observable<any> {
     return this.http.get(`inventory/api/v1/products?page=${page}&per_page=${perPage}`);
+  }
+
+  bulkUploadProducts(csvFile: File): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'text/csv'
+    });
+
+    return this.http.post('inventory/api/v1/products/bulk-upload', csvFile, { headers });
   }
 }
